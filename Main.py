2da8795d36ae55855c6ac4e87eeca50c7fef89ef
James <jamesalford661@gmail.com>
2023-11-21 17:00:44 -0600

    Changes

--- Main.py
+++ Main.py
@@ -10,4 +10,10 @@ y = spam["Label"] # Extracts the 'Label' column from the DataFrame spam and assi
 z_train, z_test,y_train, y_test = train_test_split(z,y,test_size = 0.2) # Uses the train_test_split function from scikit-learn to split the data into training and testing sets. 
 
 cv = CountVectorizer() # tokenizing,  it counts the number of occurrences of words and saves it to cv
-features = cv.fit_transform(z_train)  
+features = cv.fit_transform(z_train)  #randomly assigns a number to each word. It counts the number of occurrences of each word, then saves it to cv
+
+model = svm.SVC() # assigns svm.SVC() to the model
+model.fit(features,y_train) # trains the model with features and y_train. Then, it checks the prediction against the y_train label and adjusts its parameters until it reaches the highest possible accuracy.
+
+features_test = cv.transform(z_test)
+print("Accuracy: {}".format(model.score(features_test,y_test)))
